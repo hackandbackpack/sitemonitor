633a5e4b0507a19663de4334f701bdb05a06b4d6
hackandbackpack <96545348+hackandbackpack@users.noreply.github.com>
2023-04-30 02:46:52 -0300

    Update sitemonitor.py

--- sitemonitor.py
+++ sitemonitor.py
@@ -1,139 +1,94 @@
-import hashlib
+import json
 import threading
 import time
-from bs4 import BeautifulSoup
 import requests
-from twilio.rest import Client
-from datetime import datetime
-import pytz
+from bs4 import BeautifulSoup
 
-def read_config():
-    config = {}
-    with open("config.txt", "r") as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            config[key] = value
+CONFIG_FILE = "config.json"
+
+# Load configuration from a JSON file
+def load_config():
+    with open(CONFIG_FILE, "r") as f:
+        config = json.load(f)
     return config
 
-def read_websites(config):
-    websites = {}
-    for line in config["websites"].split(';'):
-        url, notification = line.strip().split(',')
+# Save configuration to a JSON file
+def save_config(config):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(config, f, indent=2)
+
+# Monitor websites for changes
+def monitor_websites():
+    config = load_config()
+    for website in config['websites']:
         try:
-            response = requests.get(url)
-        except requests.exceptions.RequestException as e:
-            print(f"Error while accessing {url}: {e}")
-            continue
-        soup = BeautifulSoup(response.content, "html.parser")
-        _hash = hashlib.md5(soup.prettify().encode()).hexdigest()
-        websites[url] = {
-            'hash': _hash,
-            'notification': notification
-        }
-    return websites
+            response = requests.get(website['url'])
+            soup = BeautifulSoup(response.text, 'html.parser')
+            body_text = str(soup.body)
 
-def send_sms(config, message):
-    account_sid = config["twilio_account_sid"]
-    auth_token = config["twilio_auth_token"]
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body=message,
-        from_=config["twilio_phone_number"],
-        to=config["destination_phone_number"]
-    )
+            if 'last_content' in website and website['last_content'] and website['last_content'] != body_text:
+                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {website['url']} - {website['message']} (Change detected)")
 
-def status_update(websites, stop_event):
-    while not stop_event.is_set():
-        eastern = pytz.timezone('US/Eastern')
-        current_time = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
-        print(f"\n{current_time} - Current status of monitored websites:")
-        for url, data in websites.items():
-            print(f"{url}: {data['hash']}, Custom alert: {data['notification']}")
-        time.sleep(60)
-
-def monitor(websites, config, stop_event):
-    while not stop_event.is_set():
-        for url, data in websites.items():
-            try:
-                response = requests.get(url)
-            except requests.exceptions.RequestException as e:
-                print(f"Error while accessing {url}: {e}")
-                continue
-
-            content = response.text
-            current_hash = hashlib.md5(content.encode()).hexdigest()
-
-            if current_hash != data['hash']:
-                eastern = pytz.timezone('US/Eastern')
-                current_time = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
-                print(f"\n{current_time} - Change detected on {url}, sending SMS alert.")
-                send_sms(config, data['notification'])
-                data['hash'] = current_hash
-
-        time.sleep(180)
-
-def main():
-    config = read_config()
-    websites = read_websites(config)
-
-    stop_event = threading.Event()
-    monitor_thread = threading.Thread(target=monitor, args=(websites, config, stop_event))
-    status_thread = threading.Thread(target=status_update, args=(websites, stop_event))
-
-    monitor_thread.start()
-    status_thread.start()
-
-    while True:
-        try:
-            command = input("\nEnter a command (status/start/stop/add/remove/quit): ").strip().lower()
-
-            if command == 'status':
-                eastern = pytz.timezone('US/Eastern')
-                current_time = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
-                print(f"\n{current_time} - Current status of monitored websites:")
-                for url, data in websites.items():
-                    print(f"{url}: {data['hash']}, Custom alert: {data['notification']}")
-
-            elif command == 'start':
-                if monitor_thread.is_alive():
-                    print("Monitoring is already running.")
-                else:
-                    stop_event.clear()
-                    monitor_thread = threading.Thread(target=monitor, args=(websites, config, stop_event))
-                    monitor_thread.start()
-
-            elif command == 'stop':
-                if not monitor_thread.is_alive():
-                    print("Monitoring is not running.")
-                else:
-                    stop_event.set()
-                    monitor_thread.join()
-
-            elif command == 'add':
-                url = input("Enter the URL to monitor: ").strip()
-                notification = input("Enter the custom notification: ").strip()
-                websites[url] = {'hash': '', 'notification': notification}
-                print(f"{url} added with custom alert: {notification}")
-
-            elif command == 'remove':
-                url = input("Enter the URL to remove: ").strip()
-                if url in websites:
-                    del websites[url]
-                    print(f"{url} removed.")
-                else:
-                    print(f"{url} not found in the list of monitored websites.")
-
-            elif command == 'quit':
-                stop_event.set()
-                monitor_thread.join()
-                status_thread.join()
-                break
-
-            else:
-                print("Invalid command. Please try again.")
+            website['last_content'] = body_text
 
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error while monitoring {website['url']}: {e}")
+
+    save_config(config)
+
+# Display the status of monitored websites
+def display_status():
+    config = load_config()
+    print("\nCurrent Status -", time.strftime('%Y-%m-%d %H:%M:%S'))
+    print("---------------------------------------------------")
+    for website in config['websites']:
+        print(f"{website['url']} - {website['message']}")
+    print("---------------------------------------------------\n")
+
+# Add a website to monitor
+def add_website(url, message):
+    config = load_config()
+    config['websites'].append({"url": url, "message": message, "last_content": ""})
+    save_config(config)
+
+# Remove a website from monitoring
+def remove_website(url):
+    config = load_config()
+    config['websites'] = [website for website in config['websites'] if website['url'] != url]
+    save_config(config)
+
+# Monitor websites in a separate thread
+def monitor_websites_thread():
+    while True:
+        monitor_websites()
+        display_status()
+        time.sleep(60)
+
+# User interface for managing monitored websites
+def user_interface():
+    print("Available commands: add, remove, status, quit")
+
+    # Start monitoring websites in a separate thread
+    monitor_thread = threading.Thread(target=monitor_websites_thread)
+    monitor_thread.daemon = True
+    monitor_thread.start()
+
+    # Process user commands
+    while True:
+        command = input("Enter command: ").strip().lower()
+        if command == "add":
+            url = input("Enter the URL to monitor: ")
+            message = input("Enter the custom alert message: ")
+            add_website(url, message)
+        elif command == "remove":
+            url = input("Enter the URL to stop monitoring: ")
+            remove_website(url)
+        elif command == "status":
+            display_status()
+        elif command == "quit":
+            break
+        else:
+            print("Invalid command. Available commands: add, remove, status, quit")
 
 if __name__ == "__main__":
-    main()
+    user_interface()
